4ae24dc2c95ea7018f0d31886e6a01338e70f1dc
VladimirPak <vladpak555@gmail.com>
2021-03-08 21:36:21 +0500

    Added Methods for Employee

---
+++ Paychecks.Services/Implementation/EmployeeService.cs
@@ -0,0 +1,68 @@
+﻿using Paychecks.Entity;
+using Paychecks.Persistence;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Paychecks.Services.Implementation
+{
+    public class EmployeeService : IEmployeeService
+    {
+        //Create ref to DB
+        private readonly ApplicationDbContext _context;
+        //Constructor for DB _context
+        public EmployeeService(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        //Create employee
+        public async Task CreateAsync(Employee newEmployee)
+        {
+            await _context.Employees.AddAsync(newEmployee);
+            await _context.SaveChangesAsync();
+        }
+
+        //Get employee
+        public Employee GetById(int employeeId) => 
+            _context.Employees.Where(e => e.Id == employeeId).FirstOrDefault();
+
+        //Delete employee
+        public async Task Delete(int employeeId)
+        {
+            var employee = GetById(employeeId);
+            _context.Remove(employee);
+            await _context.SaveChangesAsync();
+        }
+
+        //Update employee
+        public async Task UpdateAsync(Employee employee)
+        {
+            _context.Update(employee);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task UpdateAsync(int id)
+        {
+            var employee = GetById(id);
+            _context.Update(employee);
+            await _context.SaveChangesAsync();
+        }
+
+        //Get all employees
+        public IEnumerable<Employee> GetAll() => _context.Employees;
+
+        //
+        public decimal StudentLoanRepaymentAmount(int id, decimal totalAmount)
+        {
+            throw new NotImplementedException();
+        }
+
+        public decimal UnionFees(int id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
